b721c1e069ceea52082b9b5c01287585b82cf3a2
Igor Bobkov <bobkovid@gmail.com>
2024-04-20 20:38:09 +0300

    Added seminar1

---
+++ .folder/sem1/task_0.py
@@ -0,0 +1,26 @@
+color = "red"
+if color == "red":
+    print("Red")
+elif color == "red":
+    print("Red")
+if color == "red":
+    print("Red")
+
+
+a = 2
+while a > 0:
+    print(a)
+    if a == 3:
+        break
+    a -= 1
+else:
+    print("End no by break")
+
+my_str = "abrakadabra"
+for element in my_str:
+    print(element, end = " ")
+
+i = 0
+while i < len(my_str):
+    print(my_str[i])
+    i += 1
