cb5cb8d67cda78b6abf66e410a95e06a1a8cbe76
Igor Bobkov <bobkovid@gmail.com>
2024-06-05 23:41:52 +0300

    Completed sem9_hw9

---
+++ .folder/sem9_hw9/task2_hw9.py
@@ -0,0 +1,14 @@
+"""
+Дан файл california_housing_train.csv.
+Найти максимальное значение переменной "households" в зоне минимального значения 
+переменной min_population и сохраните это значение в переменную max_households_in_min_population.
+Используйте модуль pandas.
+"""
+
+from pandas import read_csv
+
+data = read_csv("california_housing_test.csv")
+
+max_households_in_min_population = data[data['population'] == data['population'].min()]['households'].max()
+
+print(max_households_in_min_population)
